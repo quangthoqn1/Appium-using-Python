b31d71af05ab8e842bdb263b51cef7de3215f69f
Thọ Ngô <56387461+quangthoqn1@users.noreply.github.com>
2025-07-15 13:44:03 +0700

    passed run

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,19 +1,22 @@
 import pytest
 from appium import webdriver
+from appium.options.android import UiAutomator2Options
 
 @pytest.fixture
 def driver():
     desired_caps = {
         "platformName": "Android",
         "platformVersion": "11",
-        "deviceName": "Android Emulator",
+        "deviceName": "nightwatch-android-11",
         "automationName": "UiAutomator2",
-        "app": "/tmp/app.apk",
+        "app": "D:/Automation-Mobile/app/android/Android-MyDemoAppRN.1.3.0.build-244.apk",
     }
 
-    driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+    options = UiAutomator2Options().load_capabilities(desired_caps)
+    driver = webdriver.Remote("http://localhost:4723", options=options)
     yield driver
     driver.quit()
 
 def test_open_app(driver):
-    assert driver.is_app_installed("com.example.android")
+    driver.activate_app("com.saucelabs.mydemoapp.rn")
+    assert driver.current_package == "com.saucelabs.mydemoapp.rn"
